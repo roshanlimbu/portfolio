fe6a4151ebd551cd4bc055ddb7ca9fb5d75dfe4f
Roshan Limbu <topazkayle@gmail.com>
2022-11-13 20:00:01 +0545

    Initialized the basic routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
