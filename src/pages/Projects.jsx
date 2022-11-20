1920e71948f9d30a7f5cf62e5d7b86a7b6e5c90e
Roshan Limbu <topazkayle@gmail.com>
2022-11-20 20:58:23 +0545

    Starting with the project section

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-
+import ProjectItem from "../components/ProjectItem";
 const Projects = () => {
   return (
     <div className="porjects">
       <h1>My personal projects</h1>
-      <div className="projectList"></div>
+      <div className="projectList">
+        <ProjectItem />
+        <ProjectItem />
+        <ProjectItem />
+        <ProjectItem />
+      </div>
     </div>
   );
 };
