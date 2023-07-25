7cc4d77f1f4bb5f3ca5268de1f3e38dbb924a747
Roshan Limbu <topazkayle@gmail.com>
2023-07-25 23:15:29 +0545

    done basic styling for the nav on desktop

--- src/pages/Section/Header.jsx
+++ src/pages/Section/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import {Link, NavLink} from "react-router-dom"
+import '../Styles/Header.css'
 const Header = () => {
   return (
-    <div>
-      <nav className='nav_container'>
-          <NavLink className="nav-links" to="/">Home</NavLink>
-          <NavLink className="nav-links" to="about">About</NavLink>
-          <NavLink className="nav-links" to="contact">Contact</NavLink>
-      </nav>
-    </div>
+    <nav>
+      <div className='logo-container'>
+        <Link className='logo' to='/'>Logo</Link>
+      </div>
+      <div className='nav_container'>
+        <NavLink className="nav-links" to="/">Home</NavLink>
+        <NavLink className="nav-links" to="about">About</NavLink>
+        <NavLink className="nav-links" to="contact">Contact</NavLink>
+      </div>
+    </nav>
   )
 }
 
